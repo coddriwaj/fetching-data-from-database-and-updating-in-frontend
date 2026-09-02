d94da88167f5408bcce3ff1e9d647368040894d0
Riwaj Pudasaini <pudasainixyz@gmail.com>
2026-09-02 19:04:03 +0545

    mongodb credentials removed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/signin');
+  await mongoose.connect('mongodb url ');
   console.log('MongoDB connected successfully');
 }
 
