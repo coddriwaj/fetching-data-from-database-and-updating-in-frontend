32b9071f0057e2fad2ee86ff7e75f0973e67b58a
Riwaj Pudasaini <pudasainixyz@gmail.com>
2026-09-04 01:19:33 +0545

    Add '/about' route to fetch distinct emails

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('Sigin', userSchema);
 
 
-app.get('/', async(req, res) => {
+app.get('/about', async(req, res) => {
   const emails = await User.find({}).distinct('email');
     res.json(emails);
 
